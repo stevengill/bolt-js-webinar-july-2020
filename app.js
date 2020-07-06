25145f829cb085da110482078d6733ecad4237c1
Steve Gill <stevengill@slack-corp.com>
2020-07-06 10:45:01 -0700

    did some reordering

--- app.js
+++ app.js
@@ -66,6 +66,56 @@ const app = new App({
 //   await say('Thanks for clicking the fancy button');
 // });
 
+
+// setup shortcut in your App config page
+// add the commands scope
+// app.shortcut('launch_shortcut', async ({ shortcut, ack, context, client }) => {
+
+//   try {
+//     // Acknowledge shortcut request
+//     await ack();
+
+//     // Call the views.open method using one of the built-in WebClients
+//     const result = await client.views.open({
+//       // The token you used to initialize your app is stored in the `context` object
+//       token: context.botToken,
+//       trigger_id: shortcut.trigger_id,
+//       view: {
+//         type: "modal",
+//         title: {
+//           type: "plain_text",
+//           text: "My App"
+//         },
+//         close: {
+//           type: "plain_text",
+//           text: "Close"
+//         },
+//         blocks: [
+//           {
+//             type: "section",
+//             text: {
+//               type: "mrkdwn",
+//               text: "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
+//             }
+//           },
+//           {
+//             type: "context",
+//             elements: [
+//               {
+//                 type: "mrkdwn",
+//                 text: "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
+//               }
+//             ]
+//           }
+//         ]
+//       }
+//     });
+//   }
+//   catch (error) { 
+//     console.error(error);
+//   }
+// });
+
 // app home demo
 // app.event('app_home_opened', async ({ event, context, client }) => {
 //   console.log('app home opened');
@@ -114,54 +164,6 @@ const app = new App({
 //     }
 //   }
 //   catch (error) {
-//     console.error(error);
-//   }
-// });
-
-// setup shortcut in your App config page
-// app.shortcut('launch_shortcut', async ({ shortcut, ack, context, client }) => {
-
-//   try {
-//     // Acknowledge shortcut request
-//     await ack();
-
-//     // Call the views.open method using one of the built-in WebClients
-//     const result = await client.views.open({
-//       // The token you used to initialize your app is stored in the `context` object
-//       token: context.botToken,
-//       trigger_id: shortcut.trigger_id,
-//       view: {
-//         type: "modal",
-//         title: {
-//           type: "plain_text",
-//           text: "My App"
-//         },
-//         close: {
-//           type: "plain_text",
-//           text: "Close"
-//         },
-//         blocks: [
-//           {
-//             type: "section",
-//             text: {
-//               type: "mrkdwn",
-//               text: "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
-//             }
-//           },
-//           {
-//             type: "context",
-//             elements: [
-//               {
-//                 type: "mrkdwn",
-//                 text: "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
-//               }
-//             ]
-//           }
-//         ]
-//       }
-//     });
-//   }
-//   catch (error) { 
 //     console.error(error);
 //   }
 // });
